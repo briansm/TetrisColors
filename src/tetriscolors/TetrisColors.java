e3b481082ff67f9973923f01532e802a3cae6e8c
Brian Morales <briansmor09@gmail.com>
2017-11-06 10:05:37 -0600

    Elimine clase wnd

--- src/tetriscolors/TetrisColors.java
+++ src/tetriscolors/TetrisColors.java
@@ -15,7 +15,7 @@ public class TetrisColors {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-       wnd window1=new wnd();
+    
        
     }
 
